2b535a33cc16728e62cc9ace48db83119c4eb3f1
davioooh <davidcastelletti@yahoo.it>
2018-02-05 17:57:00 +0100

    Initial commit

---
+++ src/main/java/com/davioooh/srr/SuperRubricaRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.davioooh.srr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRubricaRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperRubricaRestServiceApplication.class, args);
+	}
+}
