3569cd8c2792b2c976f15962914a879754ea1604
davioooh <davidcastelletti@yahoo.it>
2018-02-09 14:45:52 +0100

    Implentato recupero dati

--- src/main/java/com/davioooh/srr/domain/Contact.java
+++ src/main/java/com/davioooh/srr/domain/Contact.java
@@ -1,10 +1,12 @@
 package com.davioooh.srr.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Contact {
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String phone;
--- src/main/java/com/davioooh/srr/services/ContactService.java
+++ src/main/java/com/davioooh/srr/services/ContactService.java
@@ -1,6 +1,8 @@
 package com.davioooh.srr.services;
 
 import com.davioooh.srr.domain.Contact;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,14 +10,25 @@ import java.util.List;
 @Service
 public class ContactService {
 
-    public List<Contact> getList() {
-        // TODO da implementare
-        return null;
+    private List<Contact> db;
+
+    public ContactService() {
+        db = Lists.newArrayList(
+                new Contact(Long.valueOf(1), "Mario", "Rossi", "3332345678", "m.rossi@red.it"),
+                new Contact(Long.valueOf(2), "Francesca", "Gialli", "3332233444", "francesca.g@yllw.com"),
+                new Contact(Long.valueOf(3), "Marco", "Verdi", "3334545456", "marco.verdi@vmail.com")
+
+        );
     }
 
-    public Contact getById(long id) {
-        // TODO da implementare
-        return null;
+    public List<Contact> getList() {
+        return ImmutableList.copyOf(db);
+    }
+
+    public Contact getById(Long id) {
+        return db.stream()
+                .filter(b -> b.getId().equals(id))
+                .findFirst().orElse(null);
     }
 
     public Contact save(Contact contactForm) {
@@ -23,7 +36,7 @@ public class ContactService {
         return null;
     }
 
-    public void delete(long id){
+    public void delete(long id) {
         // TODO da implementare
     }
 }
